73e747c815f04671b7335521882c9ba7608ec4c0
KimalIsaev <kimchikumnik@gmail.com>
2021-07-03 15:47:48 +0700

    rewritten to changed interface

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
-export time=$(date +"%d:%m:%Y_%T")
-export i=0
+time=$(date +"%d:%m:%Y_%H:%M:%S")
+i=0
 while read line
 do
-	export name=($(basename $3 .csv)'_'$i'_'$2'_'$time)  
-	hexdump -e '2/8 "%16x " "\n"' -n $((16 * $2)) /dev/random > rng_files/$name.txt
-	./$1 $line rng_files/$name.txt > results/$name.txt
-	python first_try.py results/$name.txt pictures/$name.png
-
+	temp_name=$(basename $3 .csv)'_'$i'_'$time
+	dd if=/dev/random of=rng_files/$temp_name.raw bs=14 count=$2
+	err_output=$($1 rng_files/$temp_name.raw $line 2>&1 > results/$temp_name.txt)
+	flow_count=$(echo -n $err_output | tr -d \n) 
+	true_name=$(basename $3 .csv)'_'$i'_'$flow_count'_'$time
+	mv results/$temp_name.txt results/$true_name.txt
+	python3 draw.py results/$true_name.txt pictures/$true_name.png
 	export i=$(($i+1))
 done < $3
