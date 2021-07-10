9e4bdac001642a9e6f203f7ef631c8c4dd7ffc4b
KimalIsaev <kimchikumnik@gmail.com>
2021-07-10 22:23:54 +0700

    script now creating directory if it didn't finds one

--- run.sh
+++ run.sh
@@ -1,13 +1,19 @@
+rng_files_dir=rng_files
+results_dir=results
+pictures_dir=pictures
+
+mkdir -p $rng_files_dir $results_dir $pictures_dir
+
 time=$(date +"%d:%m:%Y_%H:%M:%S")
 i=0
 while read line
 do
 	temp_name=$(basename $3 .csv)'_'$i'_'$time
-	dd if=/dev/random of=rng_files/$temp_name.raw bs=14 count=$2
-	err_output=$($1 rng_files/$temp_name.raw $line 2>&1 > results/$temp_name.txt)
+	dd if=/dev/random of=$rng_files_dir/$temp_name.raw bs=14 count=$2
+	err_output=$($1 $rng_files_dir/$temp_name.raw $line 2>&1 > $results_dir/$temp_name.txt)
 	flow_count=$(echo -n $err_output | tr -d \n) 
 	true_name=$(basename $3 .csv)'_'$i'_'$flow_count'_'$time
-	mv results/$temp_name.txt results/$true_name.txt
-	python3 draw.py results/$true_name.txt pictures/$true_name.png
+	mv $results_dir/$temp_name.txt $results_dir/$true_name.txt
+	python3 draw.py $results_dir/$true_name.txt $pictures_dir/$true_name.png
 	export i=$(($i+1))
 done < $3
