b0f5b4c4f4915076aea1bf275cb94b17d99b4d80
KimalIsaev <kimchikumnik@gmail.com>
2022-05-18 18:04:55 +0300

    fixed scripts, haven't fixed readme

--- draw.py
+++ draw.py
@@ -1,14 +1,25 @@
 import matplotlib.pyplot as plt
 from pathlib import Path
+import numpy as np
+import csv
 import sys
-numbers = []
-with open(Path(sys.argv[1]), "r") as f:
-    for l in f:
-        numbers.append(float(l))
 
-i = len(numbers) - 1
-while (numbers[i] == 0): i-=1
-numbers = numbers[:i+1]
+def from_filename_to_list_of_lists(filename):
+    with open(filename, 'r') as read_obj:
+        csv_reader = csv.reader(read_obj, delimiter=' ')  
+        list_of_csv = list(csv_reader)
+        return list_of_csv
 
-plt.plot(numbers)
+not_transposed = from_filename_to_list_of_lists(Path(sys.argv[1]))
+print(not_transposed)
+to_draw = list(map(lambda x:list(map(float, x)),
+        zip(*not_transposed)))
+print(to_draw)
+percision = 1
+if (len(sys.argv) == 4):
+    percision = float(sys.argv[3])
+x_values = list(percision*np.arange(0, len(to_draw[0]), 1))
+
+for y_values in to_draw:
+    plt.plot(x_values, y_values)
 plt.savefig(Path(sys.argv[2]))
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-rng_files_dir=rng_files
-results_dir=results
-pictures_dir=pictures
+parent_dir=$(dirname $1)
+rng_files_dir=$parent_dir/rng_files
+results_dir=$parent_dir/results
+pictures_dir=$parent_dir/pictures
 
 mkdir -p $rng_files_dir $results_dir $pictures_dir
 
@@ -8,13 +9,13 @@ time=$(date +"%d:%m:%Y_%H:%M:%S")
 i=0
 while read line
 do
-	temp_name=$(basename $3 .csv)'_'$i'_'$time
-	dd if=/dev/random of=$rng_files_dir/$temp_name.raw bs=14 count=$2 status='none'
+	temp_name=$(basename $4 .csv)'_'$i'_'$time
+	dd if=/dev/random of=$rng_files_dir/$temp_name.raw bs=14 count=$3 status='none'
 	err_output=$($1 $rng_files_dir/$temp_name.raw $line 2>&1 > $results_dir/$temp_name.txt)
 	flow_count=$(echo -n $err_output | tr -d \n) 
-	true_name=$(basename $3 .csv)'_'$i'_'$flow_count'_'$time
+	true_name=$(basename $4 .csv)'_'$i'_'$flow_count'_'$time
 	mv $results_dir/$temp_name.txt $results_dir/$true_name.txt
-	python3 draw.py $results_dir/$true_name.txt $pictures_dir/$true_name.png
+	python3 $2 $results_dir/$true_name.txt $pictures_dir/$true_name.png
 	export i=$(($i+1))
     echo $true_name
-done < $3
+done < $4
