834ca796c62a550818933645a06c9e28ecacebc7
KimalIsaev <kimchikumnik@gmail.com>
2022-05-18 04:19:42 +0300

    added output of names

--- run.sh
+++ run.sh
@@ -9,11 +9,12 @@ i=0
 while read line
 do
 	temp_name=$(basename $3 .csv)'_'$i'_'$time
-	dd if=/dev/random of=$rng_files_dir/$temp_name.raw bs=14 count=$2
+	dd if=/dev/random of=$rng_files_dir/$temp_name.raw bs=14 count=$2 status='none'
 	err_output=$($1 $rng_files_dir/$temp_name.raw $line 2>&1 > $results_dir/$temp_name.txt)
 	flow_count=$(echo -n $err_output | tr -d \n) 
 	true_name=$(basename $3 .csv)'_'$i'_'$flow_count'_'$time
 	mv $results_dir/$temp_name.txt $results_dir/$true_name.txt
 	python3 draw.py $results_dir/$true_name.txt $pictures_dir/$true_name.png
 	export i=$(($i+1))
+    echo $true_name
 done < $3
