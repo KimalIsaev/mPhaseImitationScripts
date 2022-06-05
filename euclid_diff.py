fddf9d0a21a6fc36d038c87949c11dc4cf31734c
KimalIsaev <kimchikumnik@gmail.com>
2022-06-05 07:25:11 +0300

    fixed kolmogorov

--- euclid_diff.py
+++ euclid_diff.py
@@ -2,9 +2,6 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import sys
 
-def cosec_sum(l):
-    return list(map(sum, list(reversed([list(reversed(list(islice(list(reversed(l)), x, None)))) for x in range(len(l))]))))
-
 def filename_to_numbers(name):
     numbers = []
     with open(Path(name), "r") as f:
@@ -26,10 +23,9 @@ min_numbers1 = filename_to_numbers(sys.argv[1])
 min_numbers2 = filename_to_numbers(sys.argv[2])
 
 max_length = max(len(min_numbers1), len(min_numbers2))
-pre_numbers1 = normalize(min_numbers1 + [0]*(max_length - len(min_numbers1) + 1))
-pre_numbers2 = normalize(min_numbers2 + [0]*(max_length - len(min_numbers2) + 1))
-numbers1 = consec_sum(pre_numbers1)
-numbers2 = consec_sum(pre_numbers2)
+numbers1 = normalize(min_numbers1 + [0]*(max_length - len(min_numbers1) + 1))
+numbers2 = normalize(min_numbers2 + [0]*(max_length - len(min_numbers2) + 1))
+
 print(kolmogorov(numbers1, numbers2))
 
 
